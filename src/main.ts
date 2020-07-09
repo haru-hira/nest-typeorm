072331e4dea5896261c8ebe2b07c45a9419c958e
admin_haru <moto55.haru13@gmail.com>
2020-07-09 19:36:12 +0900

    enabled CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
+  });
   const port = (process.env.SERVER_PORT) ? process.env.SERVER_PORT : 3000;
   await app.listen(port);
 }
