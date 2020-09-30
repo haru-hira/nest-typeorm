df370148674c1f5267f823b37f825021740f1c93
admin_haru <moto55.haru13@gmail.com>
2020-09-30 22:37:43 +0900

    swagger setting to admin API.

--- src/controller/admin.controller.ts
+++ src/controller/admin.controller.ts
@@ -1,19 +1,27 @@
-import { Controller, UseGuards, Post, Get } from '@nestjs/common';
+import { Controller, UseGuards, Post, Get, HttpCode, HttpStatus } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { AdminService } from '../service/admin.service';
 
 @Controller('admin')
+@ApiTags('adimin')
 export class AdminController {
   constructor(private readonly adminService: AdminService) {}
 
-  @UseGuards(AuthGuard('local'))
+  // @UseGuards(AuthGuard('local'))
   @Post('/login')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'ログイン/jwtトークンの取得' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'ログインに成功' })
   login(): any {
     return this.adminService.sign();
   }
 
   @UseGuards(AuthGuard('jwt'))
   @Get('/status')
+  @HttpCode(HttpStatus.OK)
+  @ApiOperation({ summary: 'jwt認証の確認' })
+  @ApiResponse({ status: HttpStatus.OK, description: 'jwt認証に成功' })
   status(): boolean {
     return true;
   }
