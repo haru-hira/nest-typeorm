c423c55d9bdb593ed9399719c342cdddfcc0534b
Haru Hira <moto55.haru13@gmail.com>
2025-01-30 16:06:12 +0900

    Create user-sample.dto.ts

---
+++ src/dto/user-sample.dto.ts
@@ -0,0 +1,53 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
+
+export class CreateUserSampleDataDTO {
+  @ApiProperty({
+    description: 'The name of user.',
+    maxLength: 100,
+  })
+  name!: string;
+
+  @ApiProperty({
+    description: 'The gender of profile.',
+    maxLength: 100,
+  })
+  gender!: string;
+
+  @ApiProperty({
+    description: 'The photo of profile.',
+    maxLength: 100,
+  })
+  photo!: string;
+
+  @ApiPropertyOptional({
+    description: 'The url of photo.',
+    maxLength: 100,
+  })
+  photoUrls?: string[];
+}
+
+export class UpdateUserSampleDataDTO {
+  @ApiPropertyOptional({
+    description: 'The name of user.',
+    maxLength: 100,
+  })
+  name?: string;
+
+  @ApiPropertyOptional({
+    description: 'The gender of profile.',
+    maxLength: 100,
+  })
+  gender?: string;
+
+  @ApiPropertyOptional({
+    description: 'The photo of profile.',
+    maxLength: 100,
+  })
+  photo?: string;
+
+  @ApiPropertyOptional({
+    description: 'The url of photo.',
+    maxLength: 100,
+  })
+  photoUrls?: string[];
+}
