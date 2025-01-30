f35b367c77ae56cadef98bf93023bc07831c356f
Haru Hira <moto55.haru13@gmail.com>
2025-01-30 11:27:51 +0900

    Update xss-sample.ts

--- src/xss-sample.ts
+++ src/xss-sample.ts
@@ -4,7 +4,8 @@ const app = express();
 
 app.get('/greet', (req, res) => {
   const name = req.query.name; // ユーザー入力をそのまま返す
-  res.send(`<h1>Hello, ${name}</h1>`); // 悪意のあるスクリプトが埋め込まれる可能性あり
+  // res.send(`<h1>Hello, ${name}</h1>`); // 悪意のあるスクリプトが埋め込まれる可能性あり
+  res.json({ "data": `<h1>Hello, ${name}</h1>` }); // 正常なコード
 });
 
 app.listen(3000, () => console.log('Server running on port 3000'));
